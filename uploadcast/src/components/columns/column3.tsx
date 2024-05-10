f016ba7fe302da36d31ddadf4fb656c2fe1ca2e0
Anant dev sigh <anantdevsingh9@gmail.com>
2024-05-10 17:56:02 +0530

    TimeLine Added

--- uploadcast/src/components/columns/column3.tsx
+++ uploadcast/src/components/columns/column3.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
 function Column3() {
-  return <div className="column3">Column 3</div>;
+  return <div className="column3"></div>;
 }
 export default Column3;
--- uploadcast/src/screens/editor_screen.tsx
+++ uploadcast/src/screens/editor_screen.tsx
@@ -1,6 +1,12 @@
 import Column2 from "../components/columns/column2";
 import Column1 from "../components/columns/column1";
 import Column3 from "../components/columns/column3";
+
+import {
+  ResizableHandle,
+  ResizablePanel,
+  ResizablePanelGroup,
+} from "../../@/components/ui/resizable";
 import React from "react";
 function EditScreen() {
   return (
@@ -8,6 +14,38 @@ function EditScreen() {
       <Column1 />
       <Column2 />
       <Column3 />
+      <ResizablePanelGroup
+        direction="vertical"
+        className="min-h-[200px] max-w-md rounded-lg border customResize"
+        style={{ position: "absolute" }}
+      >
+        <ResizablePanel
+          defaultSize={40}
+          className="customResize"
+          style={{
+            // border: "1px solid #007bff",
+            width: "67.4%",
+            left: "50vh",
+            position: "relative",
+          }}
+        >
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">Header</span>
+          </div>
+        </ResizablePanel>
+        <ResizableHandle />
+        <ResizablePanel
+          defaultSize={13}
+          maxSize={55}
+          minSize={13}
+          className="customResize"
+          style={{ borderTop: "1px solid #007bff" }}
+        >
+          <div className="flex h-full items-center justify-center p-6">
+            <span className="font-semibold">TimeLine</span>
+          </div>
+        </ResizablePanel>
+      </ResizablePanelGroup>
     </div>
   );
 }
